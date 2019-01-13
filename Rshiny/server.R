ea8c37846367ac29788ebb11880c19249195468c
kevin.stark <kevin.stark@riotinto.com>
2019-01-13 20:39:59 +1000

    Plotly initial cumulative chart

---
+++ Rshiny/server.R
@@ -0,0 +1,320 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(plotly)
+library(readr)
+library(stringr)
+library(DT)
+library(purrr)
+library(magrittr)
+library(tidyr)
+library(lubridate)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+
+  bomstationids <- reactiveVal(value = c("040094", "041405"))
+  bomstationdata <- reactiveValues()
+
+  bomstation_ledger <- reactiveVal()
+
+  # Reset input box on add
+  observeEvent(input$bomstation_add, {
+    if(nchar(input$bomstation) == 6 & !is.na(as.numeric(input$bomstation))) {
+      bomstationids(unique(append(bomstationids(), input$bomstation)))
+      message("Added ", input$bomstation, ": { ", paste0(bomstationids(), collapse = " | ") ," }")
+      updateTextInput(session, inputId = "bomstation", value = "")
+    }
+  })
+
+  # Download data and metadata for added stationids
+  observe({
+    req(bomstationids)
+    missing_ids <- bomstationids()[!bomstationids() %in% names(bomstationdata)]
+    missing_data <- purrr::map(
+      missing_ids,
+      function(station) {
+        # Trigger the data coverage image generation
+        withProgress(value = 0.1, message = "Calculating data coverage", {
+          status = httr::status_code(
+            httr::GET(
+              paste0("http://www.bom.gov.au/jsp/ncc/cdio/wData/wdata?p_stn_num=", station, "&p_display_type=holdingGraph&p_nccObsCode=136&sid=")
+            )
+          )
+          # Retrieve data coverage image as:
+          #
+
+          download_path <- paste0(station, ".zip")
+          download_flag <- TRUE
+
+          if(file.exists(download_path)) {
+            if(lubridate::interval(file.info(download_path)$mtime, lubridate::now()) / lubridate::days(1) < 1) {
+              download_flag <- FALSE
+            }
+          }
+          if(download_flag) {
+            setProgress(0.3, message = "Downloading rainfall data")
+            # Download the zipped data
+            download.file(
+              paste0(
+                "http://www.bom.gov.au",
+                rvest::html_session(
+                  paste0("http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=136&p_display_type=dailyDataFile&p_startYear=&p_c=&p_stn_num=", station)
+                ) %>%
+                  rvest::html_node(xpath = "//ul[@class='downloads']/li[2]/a") %>%
+                  rvest::html_attr("href")
+              ),
+              destfile = download_path,
+              mode = "wb"
+            )
+          }
+
+          setProgress(0.7, message = "Loading rainfall csv")
+
+          csv <- readr::read_csv(
+            unz(download_path, filename = unzip(download_path, list = TRUE) %>% filter(str_detect(Name, ".csv")) %>% pull(Name)),
+            col_names = c("bom_product", "bom_station", "year", "month", "day", "rainfall_mm", "rainfall_days", "quality"),
+            skip = 1,
+            col_types = cols(
+              bom_product = col_character(),
+              bom_station = col_character(),
+              year = col_integer(),
+              month = col_integer(),
+              day = col_integer(),
+              rainfall_mm = col_double(),
+              rainfall_days = col_integer(),
+              quality = col_character()
+            )
+          )
+
+          setProgress(0.9, message = "Loading rainfall meta")
+          txt <- readr::read_lines(
+            unz(download_path, filename = unzip(download_path, list = TRUE) %>% filter(str_detect(Name, ".txt")) %>% pull(Name))
+          )
+          meta <- list(
+            station = stringr::str_split(txt[grepl(txt, pattern = "Bureau of Meteorology station number: ")], ": ", simplify = TRUE)[[2]],
+            coverage = paste0("http://www.bom.gov.au/tmp/cdio/", station, "_136.gif"),
+            name = stringr::str_split(txt[grepl(txt, pattern = "Station name: ")], ": ", simplify = TRUE)[[2]],
+            lat = stringr::str_split(txt[grepl(txt, pattern = "Latitude \\(decimal degrees, south negative\\): ")], ": ", simplify = TRUE)[[2]],
+            lon = stringr::str_split(txt[grepl(txt, pattern = "Longitude \\(decimal degrees, east positive\\): ")], ": ", simplify = TRUE)[[2]]
+          )
+
+          setProgress(1, message = "Complete")
+          return(list(data = csv, meta = meta))
+        })
+      }
+    )
+    for(i in seq_along(missing_ids)) {
+      bomstationdata[[missing_ids[i]]] <- missing_data[[i]]
+    }
+    isolate(saveRDS(reactiveValuesToList(bomstationdata), "debug_bomstationdata.RDS"))
+  })
+
+  # Parse and reformat rainfall ledger
+  observe({
+    req(bomstationdata)
+    ledger <- bind_rows(purrr::map(names(bomstationdata), ~ bomstationdata[[.]][["data"]]))
+    ledger <- mutate(ledger, date = lubridate::ymd(paste(year, month, day, sep = "-")))
+    # Convert multi-day rain events back to daily (as average)
+    ledger %<>% arrange(bom_station, date) %>% mutate(rainfall_mm_day = rainfall_mm)
+    for (i in seq_len(nrow(ledger))) {
+      if(!is.na(ledger$rainfall_days[i])) {
+        if(ledger$rainfall_days[i] > 1) {
+          ledger$rainfall_mm_day[i:(i - ledger$rainfall_days[i] + 1)] <- ledger$rainfall_mm[i] / ledger$rainfall_days[i]
+        }
+      }
+    }
+    meta <- bind_rows(purrr::map(names(bomstationdata), ~bomstationdata[[.]][["meta"]])) %>%
+      select(-coverage) %>%
+      mutate(station = stringr::str_pad(station, 6, side = "left", pad = "0"))
+
+    ledger <- ledger %>% left_join(meta, by = c("bom_station" = "station")) %>% arrange(name)
+    bomstation_ledger(ledger)
+    saveRDS(ledger, "debug_bomstation_ledger.RDS")
+  })
+
+  # Update the available date range as new station data is added
+  observe({
+    req(bomstation_ledger)
+    updateDateRangeInput(
+      session,
+      "explore_datefilter",
+      start = min(bomstation_ledger()$date),
+      end = max(bomstation_ledger()$date),
+      min = lubridate::floor_date(min(bomstation_ledger()$date), "year"),
+      max = lubridate::ceiling_date(max(bomstation_ledger()$date), "year")
+      )
+  })
+
+  # Update metadata table with stationid info
+  output$bomstation_summary <- DT::renderDataTable({
+    req(bomstationdata)
+    message("summary: ", names(bomstationdata))
+    if(length(names(bomstationdata)) > 0) {
+      meta <- bind_rows(purrr::map(names(bomstationdata), ~ bomstationdata[[.]][["meta"]]))
+      meta %<>% mutate(coverage = paste0('<img src="', coverage, '" height="30" style="align:left;border:0px;vspace:0px"/><img src="http://www.bom.gov.au/climate/cdo/images/about/timeline.png" height="19" style="align:left;border:0px;vspace:0px"/>'))
+      meta %<>% select(station, name, lon, lat, coverage)
+
+      DT::datatable(
+        meta,
+        options = list(
+          orderClasses = TRUE,
+          columnDefs = list(list(width = '400px', targets = -1))
+        ),
+        escape = FALSE)
+    }
+  })
+
+  # Update ledger with new data
+  output$bomstation_data <- DT::renderDataTable({
+    DT::datatable(bomstation_ledger())
+  })
+
+  # Generate cumulative rainfall distribution plot
+  output$explore_cumulativeplot <- plotly::renderPlotly({
+    req(bomstationdata)
+    req(input$explore_datefilter)
+    req(input$explore_timespan)
+    cat("explore_cumulativeplot\n")
+    cat("  input$explore_datefilter: ", paste(input$explore_datefilter, collapse = " to "), "\n")
+    cat("  input$explore_timespan: ", ui_timespan_months[input$explore_timespan + 1], "\n")
+    cat("  bomstation_ledger: [", paste(dim(bomstation_ledger()), collapse = " x "), "]\n")
+
+    #bomstation_ledger <- readRDS("Rshiny/debug_bomstation_ledger.RDS")
+    #bomstationdata <- readRDS("Rshiny/debug_bomstationdata.RDS")
+
+    # Calculate a day-of-year index
+    ledger <- bomstation_ledger() %>%
+      filter(date >= lubridate::ymd(input$explore_datefilter[1])) %>%
+      filter(date <= lubridate::ymd(input$explore_datefilter[2])) %>%
+      mutate(doy = lubridate::yday(date))
+    cat("  ledger: [", paste(dim(ledger), collapse = " x "), "]\n")
+
+    # Calculate the number of cumulative sequences in the dataset
+    ledger_seq <- ledger %>%
+      distinct(bom_station, name, year, month = lubridate::month(input$explore_datefilter[2]), day = lubridate::day(input$explore_datefilter[2])) %>%
+      mutate(timespan_mon = ui_timespan_months[input$explore_timespan + 1]) %>%
+      mutate(date = lubridate::ymd(paste(year, month, day, sep = "-"))) %>%
+      mutate(date_start = date %m+% months(-ui_timespan_months[input$explore_timespan + 1])) %>%
+      tidyr::unite("id", bom_station, date, sep = "_", remove = FALSE)
+    ledger_seq$days = lubridate::interval(ledger_seq$date_start, ledger_seq$date) %/% lubridate::days(1)
+    cat("  ledger_seq: [", paste(dim(ledger_seq), collapse = " x "), "]\n")
+
+    # Split ledger by unique data sequences and timespans, calculate cumulative rainfall
+    ledger_seq_data <- purrr::map(
+      seq_len(nrow(ledger_seq)),
+      function(i) {
+        ledger %>%
+          # Filter to data for the sequence
+          filter(bom_station == ledger_seq[i, "bom_station"][[1]]) %>%
+          filter(date <= ledger_seq[i, "date"][[1]]) %>%
+          filter(date > ledger_seq[i, "date_start"][[1]]) %>%
+          # Calculate cumulative rainfall
+          filter(!is.na(rainfall_mm_day)) %>%
+          arrange(date) %>%
+          mutate(rainfall_mm_cuml = cumsum(rainfall_mm_day)) %>%
+          mutate(date_norm = max(ledger_seq$date) + (date - max(date)))
+      }
+    )
+
+    # Remove incomplete seqences
+    ledger_seq_valid <- 1 - (ledger_seq$days - purrr::map_int(ledger_seq_data, nrow)) / ledger_seq$days > 0.95
+    ledger_seq_valid[is.na(ledger_seq_valid)] <- FALSE
+    cat("  ledger_seq_valid: ", sum(ledger_seq_valid), "\n")
+    # Generate cumulative rainfall plot
+    p1 <- plot_ly(height=800)
+
+    purrr::walk(
+      seq_len(nrow(ledger_seq))[ledger_seq_valid],
+      function(i) {
+        p1 <<- add_trace(
+          p1,
+          data = ledger_seq_data[[i]],
+          x = ~date_norm,
+          y = ~rainfall_mm_cuml,
+          color = ~name,
+          type = 'scatter',
+          mode = 'lines',
+          text = paste0(ledger_seq_data[[i]]$bom_station, " ", ledger_seq_data[[i]]$name, " | ", ledger_seq_data[[i]]$date, ": ", formatC(round(ledger_seq_data[[i]]$rainfall_mm_cuml), big.mark=","), "mm"),
+          hoverinfo = 'text',
+          opacity = ifelse(ledger_seq[i, "date"][[1]] == input$explore_datefilter[2], 1, 0.5),
+          line = list(width = ifelse(ledger_seq[i, "date"][[1]] == input$explore_datefilter[2], 5, 1)),
+          showlegend = FALSE
+        )
+      }
+    )
+
+    p1 <- p1 %>% layout(xaxis = list(tickformat = '%b'))
+    if(input$explore_timespan <= 2) { p1 <- p1 %>% layout(xaxis = list(tickformat = '%d-%b')) }
+    if(input$explore_timespan >= 8) { p1 <- p1 %>% layout(xaxis = list(tickformat = '%Y')) }
+
+    ledger_seq_max = tibble(
+      bom_station = purrr::map_chr(ledger_seq_data, ~ .[1, "bom_station"][[1]]),
+      name = purrr::map_chr(ledger_seq_data, ~ .[1, "name"][[1]]),
+      rainfall_mm_cuml = suppressWarnings(purrr::map_dbl(purrr::map(ledger_seq_data, "rainfall_mm_cuml"), max, na.rm = TRUE))
+    ) %>%
+      mutate(rainfall_mm_cuml = ifelse(is.infinite(rainfall_mm_cuml), 0, rainfall_mm_cuml)) %>%
+      filter(ledger_seq_valid)
+
+    ledger_seq_dens <- with(ledger_seq_max, tapply(rainfall_mm_cuml, INDEX = name, density))
+    ledger_seq_dens <- tibble(
+      rainfall_mm_cuml = unlist(lapply(ledger_seq_dens, "[[", "x")),
+      density = unlist(lapply(ledger_seq_dens, "[[", "y")),
+      name = rep(names(ledger_seq_dens), each = length(ledger_seq_dens[[1]]$x))
+    )
+
+    # Density plot per station
+    p2 <- plot_ly(
+      ledger_seq_dens,
+      x = ~density,
+      y = ~rainfall_mm_cuml,
+      color = ~name,
+      text = paste0(ledger_seq_dens$name, ": ", formatC(round(ledger_seq_dens$rainfall_mm_cuml), big.mark=","), "mm"),
+      type = 'scatter', mode = 'lines',
+      height=800,
+      hoverinfo = 'text',
+      fill = 'tozerox'
+    ) %>% layout(
+      xaxis = list(title = "", showticklabels = FALSE, showgrid = FALSE),
+      yaxis = list(hoverformat = '.0f'),
+      showlegend = TRUE
+    )
+
+    subplot(p1, p2, nrows = 1, shareY = TRUE, widths = c(0.8, 0.2)) %>%
+      layout(
+        title = "Cumulative Rainfall & Frequency Distribution",
+        yaxis = list(rangemode = "tozero"),
+        legend = list(x = 0.1, y = 0.9)
+      )
+  })
+
+  output$explore_timespan <- renderUI({
+    # Manually build arguments for the sliderInput
+    args <- list(
+      inputId = "explore_timespan",
+      label = "Select the cumulative time-span to explore",
+      ticks = TRUE,
+      value = which(names(ui_timespan_months) == "1 year") - 1,
+      min = 1,
+      max   <- length(ui_timespan_months),
+      width = "100%"
+    )
+
+    # Build slider html
+    html  <- do.call('sliderInput', args)
+
+    # Override dom with custom tick labels
+    html$children[[2]]$attribs[['data-values']] <- paste0(names(ui_timespan_months), collapse=',');
+
+    html
+  })
+
+})
---
+++ Rshiny/ui.R
@@ -0,0 +1,101 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(plotly)
+
+# Define UI for application that draws a histogram
+dashboardPage(
+  dashboardHeader(title = "rainfall"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("data", tabName = "data", icon = icon("table")),
+      menuItem("explore", tabName = "explore", icon = icon("tint")),
+      menuItem("predict", tabName = "predict", icon = icon("line-chart")),
+      menuItem("about", tabName = "about", icon = icon("info"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(
+        tabName = "data",
+        fluidRow(
+          box(
+            title = "Instructions",
+            width = 12,
+            collapsible = TRUE,
+            collapsed = FALSE,
+            shiny::h4("Historical data"),
+            shiny::em("Add nearby BOM rainfall stations which will provide historical context for distributions and predictions."),
+            shiny::em("Find stations using the link below and searching for rainfall stations near a location/address."),
+            shiny::br(),
+            shiny::a("http://www.bom.gov.au/climate/data/index.shtml", href = "http://www.bom.gov.au/climate/data/index.shtml"),
+            shiny::br(),
+            shiny::em("Select stations which have a long history of observations, and/or multiple nearby stations which have more recent data."),
+            shiny::em("Historical data will be downloaded from the BOM website for each station selected."),
+            shiny::h4("Local observations"),
+            shiny::em("Select the closest BOM station to use as recent observations, or upload your own CSV.")
+          ),
+          box(
+            title = "BOM Stations",
+            width = 12,
+            tags$div(
+              textInput("bomstation", label = "Add Station ID", placeholder = "000000"),
+              actionButton("bomstation_add", "Add", icon = icon("plus"))
+            ),
+            tags$p(),
+            tags$div(
+              DT::dataTableOutput("bomstation_summary")
+            ),
+            footer = shiny::a("Climate Data Online, Bureau of Meteorology", href = "http://www.bom.gov.au/climate/data/index.shtml")
+          ),
+          box(
+            title = "Observations",
+            width = 12,
+            DT::dataTableOutput("bomstation_data")
+          )
+        )
+      ),
+
+      # Interactive exploration of data
+      tabItem(
+        tabName = "explore",
+        fluidRow(
+          box(
+            title = "Instructions",
+            collapsible = TRUE,
+            collapsed = FALSE,
+            width = 12,
+            shiny::h4("Cumulative rainfall profiles and distribution chart"),
+            shiny::em("These charts show the cumulative rainfall totals for the same time window over many years of history."),
+            shiny::em("The distribution of cumulative total for each historical period is shown as a density plot on the right.")
+          )
+          ,
+          box(
+            title = "Inputs",
+            collapsible = TRUE,
+            width = 12,
+            dateRangeInput("explore_datefilter", label = "Select date range to explore", startview = "year"),
+            uiOutput("explore_timespan")
+          )
+          ,
+          box(
+            title = "Cumulative Rainfall",
+            collapsible = FALSE,
+            width = 12,
+            height = 900,
+            plotlyOutput("explore_cumulativeplot")
+          )
+        )
+      )
+    )
+  )
+)
